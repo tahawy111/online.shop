f5b752a362eff9d47688c6962ff69ba5655d82c6
TAHAWY <amer.zp582@gmail.com>
2022-04-08 00:19:53 +0200

    try

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const authController = require("../controllers/auth.controller");
-const check = require("express-validator");
+const check = require("express-validator").check;
 const bodyParser = require("body-parser");
 
 router.get("/signup", authController.getSignup);
@@ -16,6 +16,7 @@ router.get("/login", authController.getLogin);
 router.post(
   "/signin",
   bodyParser.urlencoded({ extended: true }),
+  check("username").require,
   authController.postLogin
 );
 
